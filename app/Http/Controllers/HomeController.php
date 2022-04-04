e5f79047057d5a47cc27685124d3f02bd53f2d4f
PdskDev <prestadesk@gmail.com>
2022-04-04 21:30:58 +0200

    Inital setup sur Online Store/Laravel 9

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+use illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(){
+        $viewData = [];
+        $viewData['title'] = "Prestadesk - Online Store";
+        return view('home.index')->with('viewData', $viewData);
+    }
+
+    public function about(){
+
+        //$title= "About us - Online Store";
+        //$subtitle = "About us";
+        //$description = "This is an about page";
+        //$author = "Developed by: Prestadesk";
+
+        //return view('home.about')->with('title', $title)
+        // ->with('subtitle', $subtitle)
+        // ->with('description', $description)
+        // ->with('author', $author);
+        $viewData = [];
+        $viewData['title']= "About us - Online Store";
+        $viewData['subtitle'] = "About us";
+        $viewData['description'] = "This is an about page";
+        $viewData['author'] = "Developed by: Prestadesk";
+
+        return view('home.about')->with('viewData', $viewData);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/', function () {
+    //$viewData = [];
+    //$viewData['title']= "Prestadesk - Online Store";
+    //return view('home.index')->with('viewData', $viewData);
+//});
+
+//Route::get('/', [HomeController::class, 'index'])->name('home.index');
+//Route::get('/about', [HomeController::class, 'about'])->name('home.about');
+
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index');
+
+Route::get('/about', 'App\Http\Controllers\HomeController@about')->name('home.about');
